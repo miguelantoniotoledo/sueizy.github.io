5c2947c475d475150873657f451b7566b9f2cb49
sueizy <msueizy@gmail.com>
2018-07-13 00:25:42 -0300

    atualizacao das sections nossos clientes e formatacoes

--- mediato/mail/contact_me.php
+++ mediato/mail/contact_me.php
@@ -41,27 +41,37 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'http://smtp.gmail.com';  			  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'sistemas@ronnycarlos.com';         // SMTP username
-    $mail->Password = 'mm3sGsrc';                         // SMTP password
+/*    $mail->Username = 'sistemas@ronnycarlos.com';         // SMTP username
+    $mail->Password = 'mm3sGsrc';                         // SMTP password */
+    $mail->Username = 'msueizy@gmail.com';                // SMTP username
+    $mail->Password = 'Legionarios*18';                    // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 25;                                     // TCP port to connect to
+    $mail->Port = 587;                                     // TCP port to connect to
 
 	  $name = strip_tags(htmlspecialchars($_POST['name']));
 	  $email_address = strip_tags(htmlspecialchars($_POST['email']));
 	  $phone = strip_tags(htmlspecialchars($_POST['phone']));
 	  $message = strip_tags(htmlspecialchars($_POST['message']));
-
+    
     //Recipients
     $mail->setFrom('sistemas@mediatomultiagencia.com.br', 'Mediato Multiagência');
     $mail->addAddress($email, $name);                    // Add a recipient
     $mail->addReplyTo('adm@mediatomultiagencia.com.br', 'Administrativo Mediato Multiagência');
 
-    //Content
+    $to = 'msueizy@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $email_subject = "Website Contact Form:  $name";
+    $email_body = "Nova mensagem do seu site!\n\n"."Detalhes:\n\nName: $name\n\nE-mail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+    $headers = "From: adm@ronnycarlos.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+    $headers .= "Reply-To: $email_address";   
+    mail($to,$email_subject,$email_body,$headers);
+    return true;
+    
+/*    //Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Nova mensagem de e-mail!';
-    $mail->Body    = "<h1>Nova mensagem de e-mail!</h1><p>Nome: $name </p><p>E-mail: $email </p>
+    $mail->Body    = "<h1>Nova mensagem de e-mail!</h1><p>Nome: $name </p><p>E-mail: $email_adress </p>
     <p>Telefone: $phone</p><p>Mensagem: $message</p>";
- // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+ // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';*/
 
     $mail->send();
     echo 'Mensagem enviada!';

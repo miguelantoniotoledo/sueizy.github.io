d0dbc5fe09b4ea98afbe53f51de386688046c76b
sueizy <msueizy@gmail.com>
2018-07-23 17:43:09 -0300

    Alteração contato.php

--- mediato/mail/contact_me.php
+++ mediato/mail/contact_me.php
@@ -1,43 +1,26 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$origem = 'msueizy@gmail.com'
-$senha = 'Legionarios*18'
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-//Load Composer's autoloader
-require 'vendor/autoload.php';
-
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = $origem                                // SMTP username
-    $mail->Password = $senha                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
-
-    //Recipients
-    $mail->setFrom($origem, 'Mediato');
-    $mail->addReplyTo( $email_address, $name);
-
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Novo e-mail da Mediato.com.br';
-    $mail->Body    = 'Email novo! De: '$name' Contato: '$email_address','$phone' Mensagem: '$message;
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-}
+// Create the email and send the message
+$to = 'marcella_sueizy@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contato:  $name";
+$email_body = "Nova mensagem.\n\n"."Detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMenssagem:\n$message";
+$headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Responda a: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
+?>

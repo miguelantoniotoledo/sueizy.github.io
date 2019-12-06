44aaa253f6a34be37c6764581d39ca426487267a
Ronny Carlos <ronny@ronnycarlos.com>
2019-12-06 18:05:18 -0300

    Correções de JS e alteração da apresentação de clientes para carrocel
    Correções diversas nos arquivos de js, passando as funções para os devidos lugares que devem ficar; alteração de inclusões de scripts fora do lugar onde deveriam ficar; alteração na inclusão do jquery local para CDN; Correção da ordem de inserção do bundle do bootstrap.

--- Ecolutions/js/bootstrap.js
+++ Ecolutions/js/bootstrap.js
@@ -2578,26 +2578,3 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
-
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
